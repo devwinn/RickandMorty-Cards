b851cf82eef444e466eed244319ba7bb64c2ed7a
Devin Wingo <devinwingo@gmail.com>
2022-06-01 16:07:40 -0700

    added eventlisteners for index. linked enter to landing page.

---
+++ js/index-js.js
@@ -0,0 +1,43 @@
+
+
+
+const portal = document.getElementById("portal");
+const button1 = document.getElementById("toSite");
+const button2 = document.getElementById("toFart");
+
+
+
+portal.addEventListener('mouseenter', onHover);
+portal.addEventListener('mouseleave', onLeave);
+button1.addEventListener('click', landing);
+button1.addEventListener('mouseenter', bigger);
+button1.addEventListener('mouseleave', smaller);
+button2.addEventListener('mouseenter', bigger);
+button2.addEventListener('mouseleave', smaller);
+
+
+
+
+function onHover() {
+  portal.style.animationPlayState = "paused";
+}
+function onLeave() {
+  portal.style.animationPlayState = "running";
+}
+
+function landing () {
+  window.location.href = "../views/landing.html";
+}
+
+function bigger (event) {
+  let button = event.target;
+  button.style.padding = "2%";
+  button.style.backgroundColor = "rgb(50, 185, 238)";
+}
+
+function smaller (event) {
+  let button = event.target;
+  button.style.padding = "";
+  button.style.backgroundColor = "";
+}
+
